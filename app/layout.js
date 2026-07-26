a5bd588be0212c6bb68c93c5720144fce80b61d8
HaiLammm <luonghaimal@gmail.com>
2026-07-26 23:28:37 +0700

    Switch favicon from PNG to JPG

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/assets/img/favicon.png" />
+        <link rel="icon" href="/assets/img/favicon.jpg" />
         <link rel="apple-touch-icon" href="/assets/img/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
